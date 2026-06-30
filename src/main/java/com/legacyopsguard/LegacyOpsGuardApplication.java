5b6e37a79dc9f32bf6774a8eec8f5557622e3968
cottonpants <kyungjin@KyungJinui-MacBookAir.local>
2026-06-30 12:31:55 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/legacyopsguard/LegacyOpsGuardApplication.java
@@ -0,0 +1,13 @@
+package com.legacyopsguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyOpsGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyOpsGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legacyopsguard/LegacyOpsGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacyopsguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyOpsGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
